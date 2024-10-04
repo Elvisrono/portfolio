217e0806ea93109a8385a072f095fc3fe581379d
ELVIS RONO <Elvis>
2024-10-04 13:18:22 +0300

    update hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import Aos from "aos";
 import './App.css'
 import Navbar from './Navbar'
+import Hero from './components/pages/Hero'
 
 function App() {
   
-
+  useEffect(() =>{
+    Aos.init();
+  }, [])
   return (
       <div>
         <Navbar />
+        <main>
+          <Hero />
+        </main>
       </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import "remixicon/fonts/remixicon.css";
+import "aos/dist/aos.css";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
